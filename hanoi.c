ba7a91850c9d477660d11cb026a6dd0bb2003e52
Lukáš Mihál <159920563+LMihal@users.noreply.github.com>
2024-09-24 19:54:36 +0200

    Update hanoi.c
    opravené

--- hanoi.c
+++ hanoi.c
@@ -7,7 +7,7 @@ void hanoi(int n, int a, int b)
   else
   {
     hanoi(n - 1, a, 6 - a - b);
-    printf("move stone %d from %d to %d\n", n, a, b);
+    printf("presun kamen %d z %d do %d\n", n, a, b);
     hanoi(n - 1, 6 - a - b, b);
   }
 }
